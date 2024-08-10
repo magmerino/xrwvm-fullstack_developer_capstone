85c365f78219897f06bd3764f9d4e450c5c7174b
magmerino <124136372+magmerino@users.noreply.github.com>
2024-08-10 22:57:24 +0200

    Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -24,7 +24,7 @@ def initiate():
             "car_make": car_make_instances[0]},
         {"name": "Qashqai", "type": "SUV", "year": 2023,
             "car_make": car_make_instances[0]},
-        {"name": "XTRAIL", "type": "SUV", "year": 2023, 
+        {"name": "XTRAIL", "type": "SUV", "year": 2023,
             "car_make": car_make_instances[0]},
         {"name": "A-Class", "type": "SUV", "year": 2023,
             "car_make": car_make_instances[1]},
